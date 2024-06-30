5cf8b7896fa3878fcf96efbfc8560be80ed40b32
Artem Novokreshenov <novokreshenov2004@gmail.com>
2024-06-30 19:41:48 +0300

    add: main section

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,11 +1,20 @@
 import {Header} from "../../components/Header/Header";
 import './Main.scss'
 
+import mouse from '../../images/mouse.png'
+
 export function Main() {
     return(
        <div className="wrapper">
            <main className="main-screen">
                <Header/>
+               <div className="main-content">
+                   <h1>Barbershop</h1>
+                   <div className="main-content__scroll">
+                       <img src={mouse} alt="mouse"/>
+                       <p>Прокрутите вниз</p>
+                   </div>
+               </div>
            </main>
        </div>
     )
